46923d46d70657ba7f8cbbc41aaec429c0307862
ohmin839 <ohmin839.echoda.resident@gmail.com>
2022-07-10 16:11:08 +0900

    Modified a view method

--- words/views.py
+++ words/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import SampledWord
 
 def homePageView(request):
-    return HttpResponse("Hello, world!")
+    count = SampledWord.objects.count()
+    return HttpResponse(f"You have {count} words.")
